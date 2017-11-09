e360c9bcbdaa8811021075b6f4b235ec22cc57d2
Dan Morton <dancmorton@gmail.com>
2017-11-09 18:50:30 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import HelloWorld from './HelloWorld.js';
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('root'));
